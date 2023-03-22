0bbb9eff68eee657942e33a397a56953ddcc9628
Michael Belete <it.michael.belete@gmail.com>
2023-03-23 00:08:01 +0300

    fetched anime

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,67 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
+  const [data, setData] = useState(null);
+
+  const [page, setPage] = useState<number>(1);
+
+  const GRAPHQL_API = "https://graphql.anilist.co";
+  const GET_ANIME_QUERY = `
+  query ($page: Int) {
+    Page(page: $page, perPage: 20) {
+     media(type: ANIME, startDate_greater: 2020, genre_in: "Action", sort: START_DATE) {
+       siteUrl
+       title {
+         native
+       }
+     }
+    }
+   }
+  `;
+
+  const getAnimes = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(GRAPHQL_API, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          query: GET_ANIME_QUERY,
+          variables: {
+            page,
+          },
+        }),
+
+      });
+      const data = await response.json();
+      setData(data.data.Page.media);
+    } catch (error) {
+      setError(error as Error);
+    }
+    setLoading(false);
+  };
+
+
+
+  useEffect(() => {
+    getAnimes();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page])
+
+  if (loading) return <div>Loading...</div>;
+
+  if (error) return <div>{error.message}</div>;
+
+
   return (
     <div>
-      <h1>Home</h1>
+      <pre>{JSON.stringify(data)}</pre>
     </div>
   );
 }
